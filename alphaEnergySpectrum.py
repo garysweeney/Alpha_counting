b73b20e1855c6579731ad5116610ce8cbd250c7e
Gary Sweeney <59615188+garysweeney@users.noreply.github.com>
2024-10-01 16:23:57 -0400

    Commit alpha energy spectrum and emissivity

--- alphaEnergySpectrum.py
+++ alphaEnergySpectrum.py
@@ -34,8 +34,6 @@ def read_alphas(file, require_fix):
         return energy, counts
     
 # Thin PTFE sample
-thin_file1 = "./thin/P5_run1.dat"
-thin_run1_energy, thin_run1_counts = read_alphas(thin_file1, False)
 thin_file2 = "./thin/P5_run2.dat"
 thin_run2_energy, thin_run2_counts = read_alphas(thin_file2, False)
 thin_file3 = "./thin/P5_run3.dat"
@@ -45,18 +43,9 @@ thin_run4_energy, thin_run4_counts = read_alphas(thin_file4, True)
 thin_file5 = "./thin/P5_run5.dat"
 thin_run5_energy, thin_run5_counts = read_alphas(thin_file5, True)
 
-thin_energy = np.hstack([thin_run3_energy, thin_run4_energy, thin_run5_energy])
-thin_counts = np.hstack([thin_run3_counts, thin_run4_counts, thin_run5_counts])
-"""
-fig, (r1,r2,r3,r4,r5) = plt.subplots(5,1)
-r2.plot(thin_run3_energy, thin_run3_counts)
-r3.plot(thin_run4_energy, thin_run4_counts)
-r4.plot(thin_run5_energy, thin_run5_counts)
-plt.show()
+thin_energy = np.hstack([thin_run2_energy, thin_run3_energy, thin_run4_energy, thin_run5_energy])
+thin_counts = np.hstack([thin_run2_counts, thin_run3_counts, thin_run4_counts, thin_run5_counts])
 
-plt.plot(thin_energy, thin_counts)
-plt.show()
-"""
 thin_sample_livetime = 5 * 24 * 3600 # 4 days in seconds
 thin_sample_area = 9.577 # cm2
 thin_alpha_emissivity = np.zeros(4)
@@ -75,8 +64,6 @@ thin_alpha_emissivity /= thin_sample_livetime
 thin_alpha_emissivity /= thin_sample_area
 
 # Medium thickness PTFE
-med_file1 = "./medium/P_med_run1.dat"
-med_run1_energy, med_run1_counts = read_alphas(med_file1, True)
 med_file2 = "./medium/P_med_run2.dat"
 med_run2_energy, med_run2_counts = read_alphas(med_file2, True)
 med_file3 = "./medium/P_med_run3.dat"
@@ -86,25 +73,14 @@ med_run4_energy, med_run4_counts = read_alphas(med_file4, True)
 med_file5 = "./medium/P_med_run5.dat"
 med_run5_energy, med_run5_counts = read_alphas(med_file5, True)
 
-med_energy = np.hstack([med_run1_energy, med_run2_energy, med_run3_energy, med_run4_energy, med_run5_energy])
-med_counts = np.hstack([med_run1_counts, med_run2_counts, med_run3_counts, med_run4_counts, med_run5_counts])
-"""
-fig, (r1,r2,r3,r4,r5) = plt.subplots(5,1)
-r1.plot(med_run1_energy, med_run1_counts)
-r2.plot(med_run2_energy, med_run2_counts)
-r3.plot(med_run3_energy, med_run3_counts)
-r4.plot(med_run4_energy, med_run4_counts)
-r5.plot(med_run5_energy, med_run5_counts)
-plt.show()
+med_energy = np.hstack([med_run2_energy, med_run3_energy, med_run4_energy, med_run5_energy])
+med_counts = np.hstack([med_run2_counts, med_run3_counts, med_run4_counts, med_run5_counts])
 
-plt.plot(med_energy, med_counts)
-plt.show()
-"""
 med_sample_livetime = 5 * 24 * 3600 # 5 days in seconds
 med_sample_area = 8.569 # cm2
 med_alpha_emissivity = np.zeros(4)
 
-for i in range(len(thin_energy)):
+for i in range(len(med_energy)):
     if med_energy[i] > 4.95 and med_energy[i] < 6.:
         med_alpha_emissivity[0] += med_counts[i]
     if med_energy[i] >= 6. and med_energy[i] < 7.:
@@ -118,8 +94,6 @@ med_alpha_emissivity /= med_sample_livetime
 med_alpha_emissivity /= med_sample_area
 
 # Thickest PTFE sample
-thick_file1 = "./thick/P_thick_run1.dat"
-thick_run1_energy, thick_run1_counts = read_alphas(thick_file1, True)
 thick_file2 = "./thick/P_thick_run2.dat"
 thick_run2_energy, thick_run2_counts = read_alphas(thick_file2, True)
 thick_file3 = "./thick/P_thick_run3.dat"
@@ -129,14 +103,14 @@ thick_run4_energy, thick_run4_counts = read_alphas(thick_file4, True)
 thick_file5 = "./thick/P_thick_run5.dat"
 thick_run5_energy, thick_run5_counts = read_alphas(thick_file5, True)
 
-thick_energy = np.hstack([thick_run1_energy, thick_run2_energy, thick_run3_energy, thick_run5_energy])
-thick_counts = np.hstack([thick_run1_counts, thick_run2_counts, thick_run3_counts, thick_run5_counts])
+thick_energy = np.hstack([thick_run2_energy, thick_run3_energy, thick_run4_energy, thick_run5_energy])
+thick_counts = np.hstack([thick_run2_counts, thick_run3_counts, thick_run4_counts, thick_run5_counts])
 
 thick_sample_livetime = 4 * 24 * 3600 # 5 days in seconds
 thick_sample_area = 8.190 # cm2
 thick_alpha_emissivity = np.zeros(4)
 
-for i in range(len(thin_energy)):
+for i in range(len(thick_energy)):
     if thick_energy[i] > 4.95 and thick_energy[i] < 6.:
         thick_alpha_emissivity[0] += thick_counts[i]
     if thick_energy[i] >= 6. and thick_energy[i] < 7.:
@@ -152,13 +126,8 @@ thick_alpha_emissivity_error_upper = np.array([17., 7., 7., 5.])
 thick_alpha_emissivity /= thick_sample_livetime
 thick_alpha_emissivity /= thick_sample_area
 
-#thick_alpha_emissivity_error_lower /= thick_sample_livetime
-#thick_alpha_emissivity_error_lower /= thick_sample_area
-#thick_alpha_emissivity_error_upper /= thick_sample_livetime
-#thick_alpha_emissivity_error_upper /= thick_sample_area
-
 alpha_energy_bands = [5.5, 6.5, 7.5, 8.5]
-
+"""
 plt.errorbar(alpha_energy_bands, [i * 1000000000 for i in thick_alpha_emissivity], xerr=0.5, capsize=3, fmt='o', label="Thick Sample")
 plt.errorbar(alpha_energy_bands, [i * 1000000000 for i in med_alpha_emissivity], xerr=0.5, capsize=3, fmt='o', label="Medium Sample")
 plt.errorbar(alpha_energy_bands, [i * 1000000000 for i in thin_alpha_emissivity], xerr=0.5, capsize=3, fmt='o', label = "Thin Sample")
@@ -168,36 +137,24 @@ plt.xticks([5.,6.,7.,8.,9.])
 #plt.yscale("log")
 plt.legend()
 plt.show()
+"""
 
-day = [1., 2., 3., 4., 5.]
-thin_runs=[sum(thin_run1_counts),
-           sum(thin_run2_counts),
-           sum(thin_run3_counts),
-           sum(thin_run4_counts),
-           sum(thin_run5_counts),
-           ]
 
-med_runs=[sum(med_run1_counts),
-           sum(med_run2_counts),
-           sum(med_run3_counts),
-           sum(med_run4_counts),
-           sum(med_run5_counts),
-           ]
-
-thick_runs = [sum(thick_run1_counts),
-              sum(thick_run2_counts),
-              sum(thick_run3_counts),
-              sum(thick_run4_counts),
-              sum(thick_run5_counts),
-              ]
-
-plt.xlabel("Day")
-plt.ylabel("Detected Alphas")
-plt.errorbar(day, thin_runs, yerr=[np.sqrt(i) for i in thin_runs], fmt="o", capsize=3, label = "Thin Sample")
-plt.errorbar(day, med_runs, yerr=[np.sqrt(i) for i in med_runs], fmt="o", capsize=3, label = "Med Sample")
-plt.errorbar(day, thick_runs, yerr=[np.sqrt(i) for i in thick_runs], fmt="o", capsize=3, label = "Thick Sample")
-plt.xticks([1.,2.,3.,4.,5.])
+plt.hist(alpha_energy_bands, bins=len(thick_alpha_emissivity), weights=[i/sum(thick_alpha_emissivity) for i in thick_alpha_emissivity], histtype="step", label="Thick Sample")
+plt.hist(alpha_energy_bands, bins=len(med_alpha_emissivity), weights=[i/sum(med_alpha_emissivity) for i in med_alpha_emissivity], histtype="step", label="Med Sample")
+plt.hist(alpha_energy_bands, bins=len(thin_alpha_emissivity), weights=[i/sum(thin_alpha_emissivity) for i in thin_alpha_emissivity], histtype="step", label="Thin Sample")
+#plt.errorbar(alpha_energy_bands, [(i/sum(thick_alpha_emissivity)) for i in thick_alpha_emissivity], xerr=0.5, capsize=3, fmt='o', label="Thick Sample")
+#plt.errorbar(alpha_energy_bands, [(i/sum(med_alpha_emissivity)) for i in med_alpha_emissivity], xerr=0.5, capsize=3, fmt='o', label="Medium Sample")
+#plt.errorbar(alpha_energy_bands, [(i/sum(thin_alpha_emissivity)) for i in thin_alpha_emissivity], xerr=0.5, capsize=3, fmt='o', label = "Thin Sample")
+plt.xlabel("Alpha Energy (MeV)")
+plt.ylabel(r"Alpha Probability")
+plt.xticks([5.,6.,7.,8.,9.])
+plt.yscale("log")
 plt.legend()
 plt.show()
 
-print(day, thick_runs, med_runs, thin_runs)
+
+
+print([i * 1000000000 for i in thick_alpha_emissivity])
+print([i * 1000000000 for i in med_alpha_emissivity])
+print([i * 1000000000 for i in thin_alpha_emissivity])
---
+++ alphaTiming.py
@@ -0,0 +1,102 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def read_alphas(file, require_fix):
+
+    if require_fix == True:
+        data = np.genfromtxt(file, dtype=str)
+        channel = np.array(data[:,0])
+        for i in range(len(channel)):
+            channel[i] = channel[i][:-1]
+        channel = np.array(channel).astype(float)
+        counts = np.array(data[:,1]).astype(float)
+        return channel, counts
+    
+    else:
+        data = np.genfromtxt(file)
+        channel = np.array(data[:,0])
+        counts = np.array(data[:,1])
+        return channel, counts
+
+# Thin PTFE sample
+thin_file1 = "./thin/P5_run1.dat"
+thin_run1_energy, thin_run1_counts = read_alphas(thin_file1, False)
+thin_file2 = "./thin/P5_run2.dat"
+thin_run2_energy, thin_run2_counts = read_alphas(thin_file2, False)
+thin_file3 = "./thin/P5_run3.dat"
+thin_run3_energy, thin_run3_counts = read_alphas(thin_file3, True)
+thin_file4 = "./thin/P5_run4.dat"
+thin_run4_energy, thin_run4_counts = read_alphas(thin_file4, True)
+thin_file5 = "./thin/P5_run5.dat"
+thin_run5_energy, thin_run5_counts = read_alphas(thin_file5, True)
+
+# Medium thickness PTFE
+med_file1 = "./medium/P_med_run1.dat"
+med_run1_energy, med_run1_counts = read_alphas(med_file1, True)
+med_file2 = "./medium/P_med_run2.dat"
+med_run2_energy, med_run2_counts = read_alphas(med_file2, True)
+med_file3 = "./medium/P_med_run3.dat"
+med_run3_energy, med_run3_counts = read_alphas(med_file3, True)
+med_file4 = "./medium/P_med_run4.dat"
+med_run4_energy, med_run4_counts = read_alphas(med_file4, True)
+med_file5 = "./medium/P_med_run5.dat"
+med_run5_energy, med_run5_counts = read_alphas(med_file5, True)
+
+# Thickest PTFE sample
+thick_file1 = "./thick/P_thick_run1.dat"
+thick_run1_energy, thick_run1_counts = read_alphas(thick_file1, True)
+thick_file2 = "./thick/P_thick_run2.dat"
+thick_run2_energy, thick_run2_counts = read_alphas(thick_file2, True)
+thick_file3 = "./thick/P_thick_run3.dat"
+thick_run3_energy, thick_run3_counts = read_alphas(thick_file3, True)
+thick_file4 = "./thick/P_thick_run4.dat"
+thick_run4_energy, thick_run4_counts = read_alphas(thick_file4, True)
+thick_file5 = "./thick/P_thick_run5.dat"
+thick_run5_energy, thick_run5_counts = read_alphas(thick_file5, True)
+
+day = [1., 2., 3., 4., 5.]
+thin_runs=[sum(thin_run1_counts),
+           sum(thin_run2_counts),
+           sum(thin_run3_counts),
+           sum(thin_run4_counts),
+           sum(thin_run5_counts)]
+
+med_runs=[sum(med_run1_counts),
+           sum(med_run2_counts),
+           sum(med_run3_counts),
+           sum(med_run4_counts),
+           sum(med_run5_counts)]
+
+thick_runs = [sum(thick_run1_counts),
+              sum(thick_run2_counts),
+              sum(thick_run3_counts),
+              sum(thick_run4_counts),
+              sum(thick_run5_counts)]
+
+
+# Cut the first day
+thin_counts = thin_runs[1:]
+thin_avg_count = np.average(thin_counts)
+print(thin_avg_count)
+med_counts = med_runs[1:]
+med_avg_count = np.mean(med_counts)
+print(med_avg_count)
+thick_counts = thick_runs[1:]
+thick_avg_count = np.average(thick_counts)
+print(thick_avg_count)
+plt.xlabel("Day")
+plt.ylabel("Detected Alphas")
+plt.errorbar(day, thin_runs, yerr=[np.sqrt(i) for i in thin_runs], fmt="o", capsize=3, label = "Thin Sample", color="blue")
+plt.fill_between(day, thin_avg_count-np.sqrt(thin_avg_count), thin_avg_count+np.sqrt(thin_avg_count), alpha=0.3, color="blue")
+plt.axhline(thin_avg_count, color="blue")
+
+plt.errorbar(day, med_runs, yerr=[np.sqrt(i) for i in med_runs], fmt="o", capsize=3, label = "Med Sample", color="green")
+plt.fill_between(day, med_avg_count-np.sqrt(med_avg_count), med_avg_count+np.sqrt(med_avg_count), alpha=0.3, color="green")
+plt.axhline(med_avg_count, color="green")
+
+plt.errorbar(day, thick_runs, yerr=[np.sqrt(i) for i in thick_runs], fmt="o", capsize=3, label = "Thick Sample", color="red")
+plt.fill_between(day, thick_avg_count-np.sqrt(thick_avg_count), thick_avg_count+np.sqrt(thick_avg_count), alpha=0.3, color="red")
+plt.axhline(thick_avg_count, color="red")
+plt.xticks([1.,2.,3.,4.,5.])
+plt.legend()
+plt.show()
